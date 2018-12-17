9d25b6eac8c7b1e90b67496d1fe82af229ec167e
gali <gbmatrix@gmail.com>
2018-12-17 02:55:37 +0200

    update migrations

--- app.rb
+++ app.rb
@@ -13,6 +13,8 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-    @barbers = Barber.all 
+    #@barbers = Barber.all
+    @barbers = Barber.order "created_at DESC"
+     
 	erb :index
 end
--- db/migrate/20181216161435_create_clients.rb
+++ db/migrate/20181216161435_create_clients.rb
@@ -1,7 +1,7 @@
 class CreateClients < ActiveRecord::Migration[5.2]
   def change
 
-  	  create_table :client do |t|   #will create table if it doesn't exist
+  	  create_table :clients do |t|   #will create table if it doesn't exist
           t.text :name
           t.text :phone
           t.text :datestamp
